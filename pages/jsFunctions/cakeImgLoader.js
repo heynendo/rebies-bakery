db07c6d97e9835d37268016f6466f4a02e58c2a1
heynendo <heynen.donovan@gmail.com>
2025-05-26 11:20:12 -0500

    Updated photos and text, updated form email, other small changes

--- pages/jsFunctions/cakeImgLoader.js
+++ pages/jsFunctions/cakeImgLoader.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imageContainer = document.getElementById('cake-images');
-    const totalImages = 20;
+    const totalImages = 16;
     const startingRow = 1;
     const possibleExtensions = ['jpeg', 'jpg', 'png', 'webp', 'gif'];
     const fallbackImage = '../images/cakes/cake-img1.jpeg';
