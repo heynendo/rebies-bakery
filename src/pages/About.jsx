771732db43f2d2d30c2af8ae6976206a8adc5b20
heynendo <heynen.donovan@gmail.com>
2026-06-16 18:15:10 -0500

    Updated cake photos section.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,6 +18,7 @@ function About(){
                 <img src={marieHeadshot} />
                 <p>Running a small baked goods business has allowed me to turn my love for baking into something people truly enjoy. Every day is a mix of creativity and hard work as I craft delicious treats that bring a smile to my customers. Though the hours can be long, the satisfaction of seeing others appreciate my work makes it all worthwhile.</p>
             </div>
+            <ImageGallery sectionKey="newCustomcakes"/>
             <ImageGallery sectionKey="customcakes"/>
             <ImageGallery sectionKey="cupcakes"/>
             <ImageGallery sectionKey="specialdesserts"/>
